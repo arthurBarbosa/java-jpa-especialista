e0a428693f66d48f9506bb69df5750b9b9b1c003
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-06 16:32:23 -0400

    inserindo o primeiro objeto

--- src/test/java/com/abcode/ecommerce/iniciandocomjpa/OperacoesComTransacao.java
+++ src/test/java/com/abcode/ecommerce/iniciandocomjpa/OperacoesComTransacao.java
@@ -1,2 +1,42 @@
-package com.abcode.ecommerce.iniciandocomjpa;public class OperacoesComTransacao {
+package com.abcode.ecommerce.iniciandocomjpa;
+
+import com.abcode.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class OperacoesComTransacao extends EntityManagerTest  {
+
+    @Test
+    public void inserirOPrimeiroObjeto() {
+        Produto produto = new Produto();
+
+        produto.setId(2);
+        produto.setNome("Câmera Canon");
+        produto.setDescricao("A melhor definição para suas fotos.");
+        produto.setPreco(new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+    }
+
+    @Test
+    public void abrirEFecharATransacao() {
+//        Produto produto = new Produto(); // Somente para o método não mostrar erros.
+
+        entityManager.getTransaction().begin();
+
+//        entityManager.persist(produto);
+//        entityManager.merge(produto);
+//        entityManager.remove(produto);
+
+        entityManager.getTransaction().commit();
+    }
 }
