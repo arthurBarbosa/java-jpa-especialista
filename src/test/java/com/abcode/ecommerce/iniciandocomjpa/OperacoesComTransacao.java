90ff7d824634bf0abbf028a055f66a673207da27
Arthur Barbosa <arthur.barbosa@itbam.org.br>
2020-10-06 16:38:40 -0400

    removendo o primeiro objeto

--- src/test/java/com/abcode/ecommerce/iniciandocomjpa/OperacoesComTransacao.java
+++ src/test/java/com/abcode/ecommerce/iniciandocomjpa/OperacoesComTransacao.java
@@ -9,6 +9,21 @@ import java.math.BigDecimal;
 public class OperacoesComTransacao extends EntityManagerTest  {
 
     @Test
+    public void removerObjeto() {
+        Produto produto = entityManager.find(Produto.class, 3);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(produto);
+        entityManager.getTransaction().commit();
+
+//        entityManager.clear(); Não é necessário na asserção para operação de remoção.
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, 3);
+        Assert.assertNull(produtoVerificacao);
+    }
+
+
+    @Test
     public void inserirOPrimeiroObjeto() {
         Produto produto = new Produto();
 
