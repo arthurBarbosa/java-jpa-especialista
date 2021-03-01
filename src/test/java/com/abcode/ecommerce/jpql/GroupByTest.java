b64d70e0a84bac5e4aaf77eae5567e97b2ea54c3
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-03-01 13:50:31 -0400

    total de vendas por dia e categoria

--- src/test/java/com/abcode/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/abcode/ecommerce/jpql/GroupByTest.java
@@ -68,4 +68,25 @@ public class GroupByTest extends EntityManagerTest {
 
         lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
     }
+
+    @Test
+    public void totaldeVendasPorDiaECategoria() {
+
+        String jpql = "select " +
+                "concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao))," +
+                "concat(c.nome, ': ', sum (ip.precoProduto)) " +
+                "from ItemPedido ip " +
+                "join ip.pedido p " +
+                "join ip.produto pro " +
+                "join pro.categorias c " +
+                "group by year(p.dataCriacao), month(p.dataCriacao)";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
 }
