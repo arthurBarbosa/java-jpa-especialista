ccc55b3d5c92fa983128ad9a77f82496b9ff4b8b
Arthur Barbosa <arthurbruno03@gmail.com>
2021-04-20 08:48:56 -0300

    consulta criteria usando expressoes null e isEmpty

--- src/test/java/com/abcode/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
+++ src/test/java/com/abcode/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -3,6 +3,8 @@ package com.abcode.ecommerce.criteria;
 import com.abcode.ecommerce.iniciandocomjpa.EntityManagerTest;
 import com.abcode.ecommerce.model.Cliente;
 import com.abcode.ecommerce.model.Cliente_;
+import com.abcode.ecommerce.model.Produto;
+import com.abcode.ecommerce.model.Produto_;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -25,6 +27,49 @@ public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
 
         TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
         List<Cliente> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsNull(){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.isNull(root.get(Produto_.FOTO)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+    }
+
+    @Test
+    public void usarIsNullProdutoSemCategoria(){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.isEmpty(root.get(Produto_.CATEGORIAS)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarIsNullProdutoComCategoria(){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.isNotEmpty(root.get(Produto_.CATEGORIAS)));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
         Assert.assertFalse(lista.isEmpty());
     }
 }
