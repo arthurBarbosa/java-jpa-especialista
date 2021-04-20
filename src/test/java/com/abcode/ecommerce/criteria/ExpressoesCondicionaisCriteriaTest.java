b95c8bffb8189d74b14247f07f8005243a86d743
Arthur Barbosa <arthurbruno03@gmail.com>
2021-04-20 08:17:24 -0300

    consulta criteria usando like

---
+++ src/test/java/com/abcode/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -0,0 +1,30 @@
+package com.abcode.ecommerce.criteria;
+
+import com.abcode.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.abcode.ecommerce.model.Cliente;
+import com.abcode.ecommerce.model.Cliente_;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.like(root.get(Cliente_.NOME), "%a%"));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Cliente> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
