7baa2ee7cc922a6a16fee32da459136b1042d68d
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-05-06 08:29:04 -0400

    buscando pedido pelo id do produto com criteria

--- src/test/java/com/abcode/ecommerce/criteria/PathExpressionTest.java
+++ src/test/java/com/abcode/ecommerce/criteria/PathExpressionTest.java
@@ -1,15 +1,18 @@
 package com.abcode.ecommerce.criteria;
 
 import com.abcode.ecommerce.iniciandocomjpa.EntityManagerTest;
+import com.abcode.ecommerce.model.*;
 import com.abcode.ecommerce.model.Cliente_;
-import com.abcode.ecommerce.model.Pedido;
+import com.abcode.ecommerce.model.ItemPedido_;
 import com.abcode.ecommerce.model.Pedido_;
+import com.abcode.ecommerce.model.Produto_;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -30,6 +33,25 @@ public class PathExpressionTest extends EntityManagerTest {
         List<Pedido> lista = typedQuery.getResultList();
 
         Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void buscarPedidosComProdutoDeIDIgual1Exercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, ItemPedido> itemPedidoJoin = root.join(Pedido_.ITENS);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+                criteriaBuilder.equal(
+                        itemPedidoJoin.get(ItemPedido_.PRODUTO).get(Produto_.ID), 1));
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+
     }
 
 
