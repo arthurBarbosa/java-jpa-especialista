5123136af4890ffe36ca6097068b5f8184426735
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-05-06 08:48:55 -0400

    agrupando total de vendas por cliente

--- src/test/java/com/abcode/ecommerce/criteria/PathExpressionTest.java
+++ src/test/java/com/abcode/ecommerce/criteria/PathExpressionTest.java
@@ -10,10 +10,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 public class PathExpressionTest extends EntityManagerTest {
@@ -53,6 +50,69 @@ public class PathExpressionTest extends EntityManagerTest {
         Assert.assertFalse(lista.isEmpty());
 
     }
+
+    @Test
+    public void agruparResultadoComFuncoes() {
+//         Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        Expression<Integer> anoCriacaoPedido = criteriaBuilder
+                .function("year", Integer.class, root.get(Pedido_.dataCriacao));
+        Expression<Integer> mesCriacaoPedido = criteriaBuilder
+                .function("month", Integer.class, root.get(Pedido_.dataCriacao));
+        Expression<String> nomeMesCriacaoPedido = criteriaBuilder
+                .function("monthname", String.class, root.get(Pedido_.dataCriacao));
+
+        Expression<String> anoMesConcat = criteriaBuilder.concat(
+                criteriaBuilder.concat(anoCriacaoPedido.as(String.class), "/"),
+                nomeMesCriacaoPedido
+        );
+
+        criteriaQuery.multiselect(
+                anoMesConcat,
+                criteriaBuilder.sum(root.get(Pedido_.total))
+        );
+
+        criteriaQuery.groupBy(anoCriacaoPedido, mesCriacaoPedido);
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr -> System.out.println("Ano/Mês: " + arr[0] + ", Sum: " + arr[1]));
+    }
+
+    @Test
+    public void agruparResultado03Exercicio() {
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<ItemPedido> root = criteriaQuery.from(ItemPedido.class);
+        Join<ItemPedido, Pedido> joinPedido = root.join(ItemPedido_.pedido);
+        Join<Pedido, Cliente> joinPedidoCliente = joinPedido.join(Pedido_.cliente);
+
+        criteriaQuery.multiselect(
+                joinPedidoCliente.get(Cliente_.nome),
+                criteriaBuilder.sum(root.get(ItemPedido_.precoProduto))
+        );
+
+        criteriaQuery.groupBy(joinPedidoCliente.get(Cliente_.id));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+
+        lista.forEach(arr -> System.out.println("Nome cliente: " + arr[0] + ", Sum: " + arr[1]));
+    }
+
 
 
 }
